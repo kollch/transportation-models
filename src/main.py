ada7cd2fb1fa8e9f8e8b86ae70c77ed2ac5a190f
Charles Koll <charles@kollonline.com>
2019-01-14 18:47:42 -0800

    Remove custom websocket implementation and replace with a library with ssl

--- src/main.py
+++ src/main.py
@@ -1,13 +1,13 @@
 # Needed for connection to frontend
 import asyncio
-import re
-import logging
+import ssl
+import websockets
 import json
-from hashlib import sha1
-from base64 import b64encode
 
 from vehicles import Vehicle, CAV, HV
 from infrastructure import Infrastructure, Intersection, Road
+
+secure = False
 
 class InvisibleHand():
     """Runs everything like the clock and spawning of vehicles;
@@ -32,6 +32,7 @@ class InvisibleHand():
         for i in range(6):
             frame = get_frame_data("testframes.json", i)
             await self.gui.send_frame(frame)
+        # Specify end of frames
         await self.gui.send_frame(None)
         return
 
@@ -43,89 +44,12 @@ class InvisibleHand():
 
 class Connection():
     """Handles a connection with the GUI"""
-    def __init__(self, reader, writer):
-        self.reader = reader
-        self.writer = writer
-        self.addr = writer.get_extra_info('peername')
-
-    async def establish(self):
-        """Make connection with GUI (using websockets)"""
-        data = await self.reader.readuntil(b'\r\n\r\n')
-        message = data.decode()
-        regex = re.search('Sec-WebSocket-Key: (.+)\r\n', message, re.IGNORECASE)
-        key = regex.group(1)
-        temp = (key + '258EAFA5-E914-47DA-95CA-C5AB0DC85B11').encode()
-        accept_key = b64encode(sha1(temp).digest()).decode()
-        header = 'HTTP/1.1 101 Switching Protocols\r\n'
-        header += 'Upgrade: websocket\r\n'
-        header += 'Connection: Upgrade\r\n'
-        header += 'Sec-WebSocket-Accept: ' + accept_key + '\r\n\r\n'
-        encoded_header = header.encode()
-        self.writer.write(encoded_header)
-        await self.writer.drain()
-        print("Connection established with", self.addr[1], "from", self.addr[0])
+    def __init__(self, websocket, path):
+        self.websocket = websocket
+        #self.addr = writer.get_extra_info('peername')
 
     async def get_parameters(self):
-        """Get parameters from GUI and store them"""
-        # Diagram given by the document detailing Websockets, RFC6455:
-        #  0                   1                   2                   3
-        #  0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1
-        # +-+-+-+-+-------+-+-------------+-------------------------------+
-        # |F|R|R|R| opcode|M| Payload len |    Extended payload length    |
-        # |I|S|S|S|  (4)  |A|     (7)     |             (16/64)           |
-        # |N|V|V|V|       |S|             |   (if payload len==126/127)   |
-        # | |1|2|3|       |K|             |                               |
-        # +-+-+-+-+-------+-+-------------+ - - - - - - - - - - - - - - - +
-        # |     Extended payload length continued, if payload len == 127  |
-        # + - - - - - - - - - - - - - - - +-------------------------------+
-        # |                               |Masking-key, if MASK set to 1  |
-        # +-------------------------------+-------------------------------+
-        # | Masking-key (continued)       |          Payload Data         |
-        # +-------------------------------- - - - - - - - - - - - - - - - +
-        # :                     Payload Data continued ...                :
-        # + - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - +
-        # |                     Payload Data continued ...                |
-        # +---------------------------------------------------------------+
-        data = await self.reader.readexactly(2)
-        # Generate a list of ints representing half-bytes
-        intlist = [int(x, 16) for x in data.hex()]
-        #print("Data:", ' '.join([hex(x) for x in data]))
-        if intlist[0] == 8:
-            fin = True
-        elif intlist[0] == 0:
-            fin = False
-        else:
-            print("Error: RSV1 or RSV2 or RSV3 not set to 0.")
-            raise ValueError
-        if intlist[1] != 0 and intlist[1] != 1 and intlist[1] != 8:
-            print("Error: don't know the opcode '" + str(intlist[1]) + "'.")
-            raise ValueError
-        elif intlist[1] == 8:
-            print("Client asking to close connection")
-            return None
-        if intlist[2] >= 8:
-            masked = True
-            intlist[2] -= 8
-        else:
-            masked = False
-        payload_length = 16 * intlist[2] + intlist[3]
-        if payload_length == 126:
-            data = await self.reader.readexactly(2)
-            payload_length = int.from_bytes(data, byteorder='big')
-        elif payload_length == 127:
-            data = await self.reader.readexactly(8)
-            payload_length = int.from_bytes(data, byteorder='big')
-        if masked:
-            mask = await self.reader.readexactly(4)
-        data = await self.reader.readexactly(payload_length)
-        if masked:
-            # Octet i of the transformed data is the XOR of
-            # octet i of the original data with octet at index
-            # i modulo 4 of the masking key
-            unmasked_data = [data[i] ^ mask[i % 4] for i in range(len(data))]
-            payload_str = bytearray(unmasked_data).decode()
-        else:
-            payload_str = data.decode()
+        payload_str = await self.websocket.recv()
         # Now convert the payload string to a json object
         payload = json.loads(payload_str)
 
@@ -135,31 +59,14 @@ class Connection():
     async def send_frame(self, json_data):
         """Send frame from json data to GUI"""
         data = json.dumps(json_data)
-        frame = [129]
-        if 125 < len(data) < 65536:
-            frame.append(126)
-            frame.extend(len(data).to_bytes(2, byteorder='big'))
-        elif len(data) >= 65536:
-            frame.append(127)
-            frame.extend(len(data).to_bytes(8, byteorder='big'))
-        else:
-            frame.append(len(data))
-        self.writer.write(bytearray(frame) + data.encode())
-        await self.writer.drain()
+        await self.websocket.send(data)
 
-    def close(self):
-        """Close connection with GUI"""
-        print("Closing the client socket")
-        self.writer.close()
-
-async def main(reader, writer):
+async def main(websocket, path):
     """Start the program with a connected frontend"""
-    connect = Connection(reader, writer)
-    await connect.establish()
+    connect = Connection(websocket, path)
     await connect.get_parameters()
     run = InvisibleHand(connect)
     await run.build_frames()
-    connect.close()
 
 def get_frame_data(file_name, frame):
     """Temporary function to read json from a file;
@@ -169,9 +76,16 @@ def get_frame_data(file_name, frame):
         data = json.load(json_file)
         return data[frame]
 
+# Start server with or without ssl
+if secure:
+    ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
+    ssl_context.load_cert_chain('cert/localhost.crt', 'cert/localhost.key')
+else:
+    ssl_context = None
+start_server = websockets.serve(main, 'localhost', 8888, ssl=ssl_context)
+
 loop = asyncio.get_event_loop()
-coro = asyncio.start_server(main, '127.0.0.1', 8888, loop=loop)
-server = loop.run_until_complete(coro)
+server = loop.run_until_complete(start_server)
 
 # Serve requests until Ctrl+C is pressed
 socket_name = server.sockets[0].getsockname()
