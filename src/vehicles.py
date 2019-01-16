07d2be66ecf36ade0fa781eb42223eeab3d8a70c
gushengjun <gush@oregonstate.edu>
2019-01-15 22:54:53 -0800

    fix some syntax errors, fix some pep8 arguments, still has few arguments

--- src/vehicles.py
+++ src/vehicles.py
@@ -1,29 +1,44 @@
-import random;
+#!/usr/bin/env python
+"""This script prompts a user to enter a message to encode or decode
+using a classic Caeser shift substitution (3 letter shift)"""
 
+import random
 
-class Vehicle():
+class Vehicle:
     """Includes CAVs and HVs"""
-    def __init__(self, length, location, destination):
-        self.length = length;
-        self.location = location;
-        self.destination = destination;
+    def __init__(self, length, location, destination, speed, accel):
+        self.length = length
+        self.location = location
+        self.destination = destination
+        self.speed = speed
+        self.accel = accel
 
-    def decide_move():
+    def decide_move(self):
         """Determines move. Required to be implemented in CAV and HV"""
-        raise NotImplementedError;
+        raise NotImplementedError
+
+#pep8 required 2 public methods?
+    def decide_other_maybe(self):
+        raise NotImplementedError
 
 class CAV(Vehicle):
     """Connected autonomous vehicles
     Connect, decide action, move
     """
-    def connect():
+    def connect(self, cavs_in_range, current_location):
         """Gets info from CAVs within range"""
-        for CAV in cavs_in_range(current_location, 3000):
-            info = CAV.give_info();
-        return;
+        self.location = current_location
 
-    def give_info():
-        return [location, speed, accel];
+        for cav in cavs_in_range(current_location, 3000):
+            info = cav.give_info() ##?????
+        return info  ##??????????
+
+    def give_info(self, location, speed, accel):
+        self.location = location
+        self.speed = speed
+        self.accel = accel
+
+        return [location, speed, accel]
 
     def react_time():
         """Randomly-generated time it will take to react - for CAVs
@@ -31,21 +46,25 @@ class CAV(Vehicle):
         program should account for the small amount of reaction time
         CAVs have
         """
-        ##sampleNo = 1  """set the samplenumber as 1, not sure for average number and sigma, could change later"""
-        mu = 0.01;
-        sigma = 0.2;
-        r_time = random.gauss(mu, sigma);
-        
-        """r_time is randomly number but sometimes it will has negative number"""
-        
+        ##sampleNo = 1  """set the samplenumber as 1, not sure for average number and sigma"""
+        mean = 0.01
+        sigma = 0.2
+        r_time = random.gauss(mean, sigma)
+
+        #"""r_time is randomly number but sometimes it will has negative number"""
+
         while r_time[0] < 0:
-            r_time = random.gauss(mu, sigma)
+            r_time = random.gauss(mean, sigma)
 
-        return r_time;
+        return r_time
 
-    def decide_move():
+    def decide_move(self):
         """Uses available information and determines move"""
-        return 0;
+        return 0
+
+    #pep8 required 2 public methods? we can delete this function
+    def decide_other_maybe(self):
+        return 0
 
 class HV(Vehicle):
     """Human-driven vehicles
@@ -54,18 +73,22 @@ class HV(Vehicle):
     def react_time():
         """Randomly-generated time it will take to react"""
 
-        ##sampleNo = 1;  """set the samplenumber as 1"""
-        mu = 0.5;
-        sigma = 1;
-        r_time = random.gauss(mu, sigma);
+        #sampleNo = 1;  """set the samplenumber as 1"""
+        mean = 0.5
+        sigma = 1
+        r_time = random.gauss(mean, sigma)
 
-        """r_time is randomly number but sometimes it will has negative number"""
+        #"""r_time is randomly number but sometimes it will has negative number"""
 
         while r_time[0] < 0:
-            r_time = random.gauss(mu, sigma)
-        
-        return r_time;
+            r_time = random.gauss(mean, sigma)
 
-    def decide_move():
+        return r_time
+
+    def decide_move(self):
         """Looks in immediate vicinity and determines move"""
-        return 0;
+        return 0
+        #######
+    #pep8 required 2 public methods? we can delete this function
+    def decide_other_maybe(self):
+        return 0
