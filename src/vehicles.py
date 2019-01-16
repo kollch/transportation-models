cd8f415d51be725483f77d0b13a8752c79a5d34b
gushengjun <gush@oregonstate.edu>
2019-01-15 21:32:35 -0800

    normal random instead of numpy stack

--- src/vehicles.py
+++ src/vehicles.py
@@ -1,16 +1,16 @@
-import numpy as np
+import random;
 
 
 class Vehicle():
     """Includes CAVs and HVs"""
     def __init__(self, length, location, destination):
-        self.length = length
-        self.location = location
-        self.destination = destination
+        self.length = length;
+        self.location = location;
+        self.destination = destination;
 
     def decide_move():
         """Determines move. Required to be implemented in CAV and HV"""
-        raise NotImplementedError
+        raise NotImplementedError;
 
 class CAV(Vehicle):
     """Connected autonomous vehicles
@@ -19,11 +19,11 @@ class CAV(Vehicle):
     def connect():
         """Gets info from CAVs within range"""
         for CAV in cavs_in_range(current_location, 3000):
-            info = CAV.give_info()
-        return
+            info = CAV.give_info();
+        return;
 
     def give_info():
-        return [location, speed, accel]
+        return [location, speed, accel];
 
     def react_time():
         """Randomly-generated time it will take to react - for CAVs
@@ -31,19 +31,21 @@ class CAV(Vehicle):
         program should account for the small amount of reaction time
         CAVs have
         """
-        sampleNo = 1  """set the samplenumber as 1, not sure for average number and sigma, could change later"""
-        mu = 0.01
-        sigma = 0.2
-        r_time = np.random.normal(mu, sigma, sampleNo )
-
+        ##sampleNo = 1  """set the samplenumber as 1, not sure for average number and sigma, could change later"""
+        mu = 0.01;
+        sigma = 0.2;
+        r_time = random.gauss(mu, sigma);
+        
+        """r_time is randomly number but sometimes it will has negative number"""
+        
         while r_time[0] < 0:
-            r_time = np.random.normal(mu, sigma, sampleNo )"""r_time is randomly number but sometimes it will has negative number"""
+            r_time = random.gauss(mu, sigma)
 
         return r_time;
 
     def decide_move():
         """Uses available information and determines move"""
-        return
+        return 0;
 
 class HV(Vehicle):
     """Human-driven vehicles
@@ -52,20 +54,18 @@ class HV(Vehicle):
     def react_time():
         """Randomly-generated time it will take to react"""
 
-        sampleNo = 1;  """set the samplenumber as 1"""
-        mu = 0.5
-        sigma = 1
-        r_time = np.random.normal(mu, sigma, sampleNo )
+        ##sampleNo = 1;  """set the samplenumber as 1"""
+        mu = 0.5;
+        sigma = 1;
+        r_time = random.gauss(mu, sigma);
+
+        """r_time is randomly number but sometimes it will has negative number"""
 
         while r_time[0] < 0:
-            r_time = np.random.normal(mu, sigma, sampleNo )"""r_time is randomly number but sometimes it will has negative number"""
-
+            r_time = random.gauss(mu, sigma)
+        
         return r_time;
-
-
-
-        return
 
     def decide_move():
         """Looks in immediate vicinity and determines move"""
-        return
+        return 0;
