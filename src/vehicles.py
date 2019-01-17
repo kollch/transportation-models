b568d07ea6c9b3c1de847ab914d0674ab9573368
gushengjun <gush@oregonstate.edu>
2019-01-16 16:38:31 -0800

    Remove code unrelated to feature

--- src/vehicles.py
+++ src/vehicles.py
@@ -1,43 +1,27 @@
-#!/usr/bin/env python
-"""This script prompts a user to enter a message to encode or decode
-using a classic Caeser shift substitution (3 letter shift)"""
-
 import random
 
-class Vehicle:
+class Vehicle():
     """Includes CAVs and HVs"""
-    def __init__(self, length, location, destination, speed, accel):
+    def __init__(self, length, location, destination):
         self.length = length
         self.location = location
         self.destination = destination
-        self.speed = speed
-        self.accel = accel
 
-    def decide_move(self):
+    def decide_move():
         """Determines move. Required to be implemented in CAV and HV"""
-        raise NotImplementedError
-
-#pep8 required 2 public methods?
-    def decide_other_maybe(self):
         raise NotImplementedError
 
 class CAV(Vehicle):
     """Connected autonomous vehicles
     Connect, decide action, move
     """
-    def connect(self, cavs_in_range, current_location):
+    def connect():
         """Gets info from CAVs within range"""
-        self.location = current_location
+        for CAV in cavs_in_range(current_location, 3000):
+            info = CAV.give_info()
+        return
 
-        for cav in cavs_in_range(current_location, 3000):
-            info = cav.give_info() ##?????
-        return info  ##??????????
-
-    def give_info(self, location, speed, accel):
-        self.location = location
-        self.speed = speed
-        self.accel = accel
-
+    def give_info():
         return [location, speed, accel]
 
     def react_time():
@@ -46,25 +30,11 @@ class CAV(Vehicle):
         program should account for the small amount of reaction time
         CAVs have
         """
-        ##sampleNo = 1  """set the samplenumber as 1, not sure for average number and sigma"""
-        mean = 0.01
-        sigma = 0.2
-        r_time = random.gauss(mean, sigma)
+        return 0
 
-        #"""r_time is randomly number but sometimes it will has negative number"""
-
-        while r_time[0] < 0:
-            r_time = random.gauss(mean, sigma)
-
-        return r_time
-
-    def decide_move(self):
+    def decide_move():
         """Uses available information and determines move"""
-        return 0
-
-    #pep8 required 2 public methods? we can delete this function
-    def decide_other_maybe(self):
-        return 0
+        return
 
 class HV(Vehicle):
     """Human-driven vehicles
@@ -72,23 +42,15 @@ class HV(Vehicle):
     """
     def react_time():
         """Randomly-generated time it will take to react"""
-
-        #sampleNo = 1;  """set the samplenumber as 1"""
-        mean = 0.5
+        mu = 0.5
         sigma = 1
-        r_time = random.gauss(mean, sigma)
+        r_time = random.gauss(mu, sigma)
 
-        #"""r_time is randomly number but sometimes it will has negative number"""
-
-        while r_time[0] < 0:
-            r_time = random.gauss(mean, sigma)
-
+        # r_time is randomly number but sometimes it will has negative number
+        while r_time <= 0:
+             r_time = random.gauss(mu, sigma)
         return r_time
 
-    def decide_move(self):
+    def decide_move():
         """Looks in immediate vicinity and determines move"""
-        return 0
-        #######
-    #pep8 required 2 public methods? we can delete this function
-    def decide_other_maybe(self):
-        return 0
+        return
