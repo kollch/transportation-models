899f279e86c141b8a5fd369f56fb369479b37ef5
Charles Koll <charles@kollonline.com>
2019-01-29 22:14:58 -0800

    Modify function params to reflect input data

--- src/vehicles.py
+++ src/vehicles.py
@@ -3,12 +3,14 @@ import random
 
 class Vehicle():
     """Includes CAVs and HVs"""
-    def __init__(self, length, location, destination):
+    def __init__(self, vehicle_id=None, length=20,
+                 location=None, destination=None):
+        self.vehicle_id = vehicle_id
         self.length = length
         self.location = location
         self.destination = destination
 
-    def decide_move():
+    def decide_move(self):
         """Determines move. Required to be implemented in CAV and HV"""
         raise NotImplementedError
 
@@ -17,16 +19,18 @@ class CAV(Vehicle):
     """Connected autonomous vehicles
     Connect, decide action, move
     """
-    def connect():
+    autonomous = True
+
+    def connect(self):
         """Gets info from CAVs within range"""
         for cav in cavs_in_range(current_location, 3000):
             info = cav.give_info()
         return
 
-    def give_info():
+    def give_info(self):
         return [location, speed, accel]
 
-    def react_time():
+    def react_time(self):
         """Randomly-generated time it will take to react - for CAVs
         this should be instantaneous because the timesteps in this
         program should account for the small amount of reaction time
@@ -34,7 +38,7 @@ class CAV(Vehicle):
         """
         return 0
 
-    def decide_move():
+    def decide_move(self):
         """Uses available information and determines move"""
         return
 
@@ -43,7 +47,9 @@ class HV(Vehicle):
     """Human-driven vehicles
     Decide action, move
     """
-    def react_time():
+    autonomous = False
+
+    def react_time(self):
         """Randomly-generated time it will take to react"""
         mu = 0.5
         sigma = 1
@@ -54,6 +60,6 @@ class HV(Vehicle):
             r_time = random.gauss(mu, sigma)
         return r_time
 
-    def decide_move():
+    def decide_move(self):
         """Looks in immediate vicinity and determines move"""
         return
