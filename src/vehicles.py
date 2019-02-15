5d5d179a03507068fb90ec337b1240bfde045fc1
Charles Koll <charles@kollonline.com>
2019-02-15 13:58:13 -0800

    Add vehicle visibility function

--- src/vehicles.py
+++ src/vehicles.py
@@ -1,18 +1,85 @@
 import random
+import math
 
 
 class Vehicle():
     """Includes CAVs and HVs"""
-    def __init__(self, vehicle_id=None, length=20,
-                 location=None, destination=None):
+    def __init__(self, vehicle_id=None, length=20, width=8,
+                 loc=None, dest=None, direction=0):
         self.vehicle_id = vehicle_id
         self.length = length
-        self.location = location
-        self.destination = destination
+        self.width = width
+        self.loc = loc
+        self.dest = dest
+        self.direction = direction
+
+    def angle(self, loc):
+        """Compute angle from vehicle to location"""
+        return math.atan2(loc[1] - self.loc[1], loc[0] - self.loc[0])
 
     def decide_move(self):
         """Determines move. Required to be implemented in CAV and HV"""
         raise NotImplementedError
+
+    def dist_to(self, loc):
+        """Returns the distance between the vehicle and a given
+        location"""
+        return math.hypot(loc[0] - self.loc[0], loc[1] - self.loc[1])
+
+    def can_see(self, infrastructure, cavs, hvs):
+        """Returns a list of everything that the vehicle can see"""
+        in_vision = []
+        for vehicle in sorted(cavs + hvs, key=lambda v: self.dist_to(v.loc)):
+            if vehicle is self:
+                continue
+            angle = vehicle.angle_edges(self)
+            def leq(a, b):
+                """Finds if angle a is less than angle b.
+                Needed because of wrapping at pi = -pi"""
+                if math.atan2(math.sin(a - b), math.cos(a - b)) <= 0:
+                    return True
+                return False
+            def is_visible(angle_end_val=None):
+                """Returns true if vehicle is visible, false
+                otherwise"""
+                for item in in_vision:
+                    if angle_end_val is None:
+                        low = item[1][0]
+                        mid = angle[0]
+                        angle_end = item[1][1]
+                    else:
+                        low = angle[0]
+                        mid = item[1][0]
+                        angle_end = angle_end_val
+                    if leq(low, mid) and leq(mid, angle_end):
+                        if leq(angle[1], item[1][1]):
+                            return False
+                        angle_end = item[1][1]
+                        if angle_end_val is None:
+                            return is_visible(angle_end)
+                return True
+            if is_visible():
+                in_vision.append((vehicle, angle))
+        return [i[0] for i in in_vision]
+
+    def angle_edges(self, vehicle):
+        """Returns the outermost seen angles of the vehicle from
+        the given vehicle"""
+        lsin = self.length / 2 * math.sin(self.direction)
+        lcos = self.length / 2 * math.cos(self.direction)
+        wsin = self.width / 2 * math.sin(self.direction)
+        wcos = self.width / 2 * math.cos(self.direction)
+        points = [(self.loc[0] + lcos + wsin, self.loc[1] + lsin - wcos),
+                  (self.loc[0] - lcos - wsin, self.loc[1] + wcos - lsin),
+                  (self.loc[0] + lcos - wsin, self.loc[1] + lsin + wcos),
+                  (self.loc[0] + wsin - lcos, self.loc[1] - lsin - wcos)]
+        angles = [vehicle.angle(i) for i in points]
+        min_angle = min(angles)
+        max_angle = max(angles)
+        # Check if wrapping around pi = -pi
+        if math.degrees(min_angle) < -90 and math.degrees(max_angle) > 90:
+            return [max_angle, min_angle]
+        return [min_angle, max_angle]
 
 
 class CAV(Vehicle):
@@ -20,6 +87,16 @@ class CAV(Vehicle):
     Connect, decide action, move
     """
     autonomous = True
+
+    def __str__(self):
+        return ("{CAV " + str(self.vehicle_id)
+                + "\n   Loc: " + str(self.loc)
+                + "\n  Dest: " + str(self.dest)
+                + "\n  Size: " + str(self.length) + " x " + str(self.width)
+                + "\n Angle: " + str(self.direction) + "\n}")
+
+    def __repr__(self):
+        return "(CAV " + str(self.vehicle_id) + ")"
 
     def connect(self):
         """Gets info from CAVs within range"""
@@ -48,6 +125,16 @@ class HV(Vehicle):
     Decide action, move
     """
     autonomous = False
+
+    def __str__(self):
+        return ("{HV " + str(self.vehicle_id)
+                + "\n   Loc: " + str(self.loc)
+                + "\n  Dest: " + str(self.dest)
+                + "\n  Size: " + str(self.length) + " x " + str(self.width)
+                + "\n Angle: " + str(self.direction) + "\n}")
+
+    def __repr__(self):
+        return "(HV " + str(self.vehicle_id) + ")"
 
     def react_time(self):
         """Randomly-generated time it will take to react"""
