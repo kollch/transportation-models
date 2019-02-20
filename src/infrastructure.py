01b3ba213a9c4e89448035207751a4cd6476c9ed
gushengjun <gush@oregonstate.edu>
2019-02-19 17:34:24 -0800

    Change string list to boolen list, remove else statement

--- src/infrastructure.py
+++ src/infrastructure.py
@@ -18,12 +18,10 @@ class Intersection():
 
     def road_open(self):
         """Create intersection roads list, true -- road open, false -- road close"""
-        roads_list = ['false', 'true', 'false', 'true']
+        roads_list = [False, True, False, True]
         for i in range(4): 
-            if self.roads[i] == 'null':
-                roads_list[i] = 'false'
-            else:
-                roads_list[i] = roads_list[i]
+            if self.roads[i] is None:
+                roads_list[i] = None
         return roads_list
 
 class Road():
